ae2c58ad44b841c3674b5448eac7cf3f1e076022
SteelMagnolias <56775507+SteelMagnolias@users.noreply.github.com>
2023-09-09 18:36:52 -0400

    set up basic autonomous so I can push

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/OdometryTest.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class OdometryTest extends LinearOpMode {
+
+    // initializations stuff
+
+    @Override
+    public void runOpMode() {
+        // init
+
+        waitForStart();
+
+        // start code
+        while (opModeIsActive()) {
+           // actual actions lol
+        }
+    }
+}
