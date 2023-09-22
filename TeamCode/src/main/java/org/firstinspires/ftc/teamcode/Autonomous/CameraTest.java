ca0d74ce6d5c00d18f6102c59ab611a027606377
vrluer <vrluer@optonline.net>
2023-09-22 12:05:00 -0400

    finished very very basic cameratest file recognized feild elements and adds telemetry.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/CameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/CameraTest.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
+//import importaint files from sdk
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -12,23 +13,39 @@ import java.util.List;
 
 @Autonomous(name = "CameraTest", group="Iterative OpMode")
 public class CameraTest extends LinearOpMode {
+
+    //define camera
     private WebcamName camera;
 
+    //create tensor flow processor this will only do default items and settings
     TfodProcessor tfodProcessor = TfodProcessor.easyCreateWithDefaults();
 
+    //create vision portal stream with default settings use camera and send to tfodprocessor
     VisionPortal visionPortal = VisionPortal.easyCreateWithDefaults(camera, tfodProcessor);
 
     @Override
     public void runOpMode() throws InterruptedException {
+        // gets webcam info from configuration
         WebcamName camera = hardwareMap.get(WebcamName.class, "webcam 1");
 
+        // recognize things
         List<Recognition> recognitions = tfodProcessor.getRecognitions();
 
-        for (Recognition recognition : recognitions);{
+        // list recognitions
+        for (Recognition recognition : recognitions){
 
+            //prep label
             String label = recognition.getLabel();
 
+            //prep confidence
             float confidence = recognition.getConfidence();
+
+            //add object to telemetry
+            telemetry.addLine("recognized object: " + label);
+            telemetry.addLine("confidence: " + confidence);
+
+            //update telemetry
+            telemetry.update();
         }
     }
 }
