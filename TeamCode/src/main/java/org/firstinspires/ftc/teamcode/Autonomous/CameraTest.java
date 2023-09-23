c945b6e18e90c314383473e6153a2511fce564d4
vrluer <vrluer@optonline.net>
2023-09-23 18:34:55 -0400

    finished custom tfod and vision portal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/CameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/CameraTest.java
@@ -4,12 +4,15 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.tfod.TfodProcessor;
 
 import java.util.List;
+import android.util.Size;
+
 
 @Autonomous(name = "CameraTest", group="Iterative OpMode")
 public class CameraTest extends LinearOpMode {
@@ -17,35 +20,88 @@ public class CameraTest extends LinearOpMode {
     //define camera
     private WebcamName camera;
 
-    //create tensor flow processor this will only do default items and settings
-    TfodProcessor tfodProcessor = TfodProcessor.easyCreateWithDefaults();
+    //define processor variable
+    private TfodProcessor tfod;
 
-    //create vision portal stream with default settings use camera and send to tfodprocessor
-    VisionPortal visionPortal = VisionPortal.easyCreateWithDefaults(camera, tfodProcessor);
+    private VisionPortal visionPortal;
+
+    //pick a custom model to use
+    private static final String TFOD_MODEL_ASSET = "file name";
+
+    //name of objects identified
+    private static final String[] LABELS = {
+            "heart"
+    };
+
 
     @Override
     public void runOpMode() throws InterruptedException {
-        // gets webcam info from configuration
-        WebcamName camera = hardwareMap.get(WebcamName.class, "webcam 1");
 
-        // recognize things
-        List<Recognition> recognitions = tfodProcessor.getRecognitions();
+    }
 
-        // list recognitions
-        for (Recognition recognition : recognitions){
+    //function to initilize tensorflow with prefered settings
+    private void initTfod() {
 
-            //prep label
-            String label = recognition.getLabel();
+        // start building custom tfod
+        TfodProcessor.Builder builder = new TfodProcessor.Builder();
+        //custom training modle file import
+        builder.setModelFileName(TFOD_MODEL_ASSET);
+        //get names of objects
+        builder.setModelLabels(LABELS);
+        // verifys the type of tensor flow model???? not really sure
+        builder.setIsModelTensorFlow2(true);
+        // no clue
+        builder.setIsModelQuantized(true);
+        // how many minutes of model will be used.
+        builder.setModelInputSize(300);
+        //set camera aspect ratio
+        builder.setModelAspectRatio(16.0 / 9.0);
+        //make custom tfod
+        tfod = builder.build();
 
-            //prep confidence
-            float confidence = recognition.getConfidence();
+        //start building custom vision portal
+        VisionPortal.Builder builder2 = new VisionPortal.Builder();
+       //pick a camera 
+        builder2.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        // set camera resolution
+        //this one already has precalibrated apriltag info
+        builder2.setCameraResolution(new Size(640, 480));
+        //allow you to see the camera feed on driver hub
+        builder2.enableLiveView(true);
+        // streaming bandwith
+        builder2.setStreamFormat(VisionPortal.StreamFormat.YUY2);
+        // dont auto stop camera feed
+        builder2.setAutoStopLiveView(false);
+        //add custom tfod to vision portal
+        builder2.addProcessor(tfod);
+        //build vision portal
+        visionPortal = builder2.build();
 
-            //add object to telemetry
-            telemetry.addLine("recognized object: " + label);
-            telemetry.addLine("confidence: " + confidence);
+        //must be at least 75% confident to display data
+        tfod.setMinResultConfidence(0.75f);
 
-            //update telemetry
-            telemetry.update();
+        //processor is on
+        visionPortal.setProcessorEnabled(tfod, true);
+    }
+
+    private void tfodtelemetry() {
+        List<Recognition> currentRecognitions = tfod.getRecognitions();
+        telemetry.addData("# Objects Detected", currentRecognitions.size());
+
+        for (Recognition recognition : currentRecognitions) {
+            //cordinents
+            //average x cordinents
+            double x = (recognition.getLeft() + recognition.getRight()) / 2 ;
+           //average y cordinents
+            double y = (recognition.getTop()  + recognition.getBottom()) / 2 ;
+
+            //add telemetry
+            //label and regignition %
+            telemetry.addData(recognition.getLabel(), "%s (%.0f %% Conf.)", recognition.getConfidence() * 100);
+            // label and cordinents
+            telemetry.addData("Position of ", recognition.getLabel(), "%.0f / %.0f", x, y);
+            //label and size
+            telemetry.addData("Size of ", recognition.getLabel(), "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
         }
     }
 }
