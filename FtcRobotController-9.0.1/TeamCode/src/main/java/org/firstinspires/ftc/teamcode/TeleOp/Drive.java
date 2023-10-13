650905c2285dc2e81be81e607a6deeb9b89f6978
SteelMagnolias <56775507+SteelMagnolias@users.noreply.github.com>
2023-10-13 19:35:09 -0400

    return teleop to before commit issue

--- FtcRobotController-9.0.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Drive.java
+++ FtcRobotController-9.0.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Drive.java
@@ -28,11 +28,76 @@ public class Drive extends OpMode {
 
     public void init() {
         // init
+        leftBack  = hardwareMap.get(DcMotor.class, "leftBack");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        hook = hardwareMap.get(DcMotor.class, "hook");
+        horizontalArm = hardwareMap.get(DcMotor.class, "horizontalArm");
+        verticalArm = hardwareMap.get(DcMotor.class, "verticalArm");
 
+        intakeClaw = hardwareMap.get(Servo.class, "intakeClaw");
+
+        distanceSensor = hardwareMap.get(DistanceSensor.class, "distanceSensor");
     }
 
     public void loop(){
         // main code
+
+        // gamepad 1
+        double lefty1 = -(gamepad1.left_stick_y); // this is the value of gamepad1's left joystick y value
+        double leftx1 = gamepad1.left_stick_x; // this is the value of gamepad1's left joystick x value
+        double rightx1 = gamepad1.right_stick_x; // this is the value of gamepad1's right joystick x value
+        double righty1 = (gamepad1.right_stick_y); // this the value of gamepad1's right joystick y value
+        boolean buttonUp1 = gamepad1.dpad_up; // this is the value of gamepad1's up button on the dpad
+        boolean buttonDown1 = gamepad1.dpad_down; // this is the value of gamepad1's down button on the dpad
+        boolean buttonLeft1 = gamepad1.dpad_left; // this is the value of the gamepad1's left button on the dpad
+        boolean buttonRight1 = gamepad1.dpad_right; // this is the value of the gamepad1's right button on the dpad
+        boolean lb1 = gamepad1.left_bumper; // this is the value of the gamepad1's left bumper
+        boolean rb1 = gamepad1.right_bumper; // this is the value of the gamepad1's right bumper
+        boolean a1 = gamepad1.a; // this is the value of the a button on gamepad1
+        boolean x1 = gamepad1.x; // this is the value of the x button on gamepad1
+        boolean y1 = gamepad1.y; // this is the value of the y button on gamepad1
+        boolean b1 = gamepad1.b; // this is the value of the b button on gamepad1
+
+        // gamepad 2
+        double lefty2 = -(gamepad2.left_stick_y); // this is the value of gamepad2's left joystick y value
+        double leftx2 = gamepad2.left_stick_x; // this is the value of gamepad2's left joystick x value
+        double rightx2 = gamepad2.right_stick_x; // this the value of gamepad2's right joystick x value
+        double righty2 = (gamepad2.right_stick_y); // this is the value of gamepad2's right joystick y value
+        boolean a2 = gamepad2.a; // this is the value of the a button on gamepad2
+        boolean x2 = gamepad2.x; // this is the value of the x button on gamepad2
+        boolean y2 = gamepad2.y; // this is the value of the y button on gamepad2
+        boolean b2 = gamepad2.b; // this is the value of the b button on gamepad2
+
+        // telemetry
+        telemetry.addData("Gamepad:", 1);
+        telemetry.addData("lefty1", lefty1);
+        telemetry.addData("leftx1", leftx1);
+        telemetry.addData("righty1", righty1);
+        telemetry.addData("rightx1", rightx1);
+        telemetry.addData("buttonUp1", buttonUp1);
+        telemetry.addData("buttonDown1", buttonDown1);
+        telemetry.addData("buttonLeft1", buttonLeft1);
+        telemetry.addData("buttonRight1", buttonRight1);
+        telemetry.addData("lb1", lb1);
+        telemetry.addData("rb1", rb1);
+        telemetry.addData("a1", a1);
+        telemetry.addData("b1", b1);
+        telemetry.addData("x1", x1);
+        telemetry.addData("y1", y1);
+
+        telemetry.addData("Gamepad:", 2);
+        telemetry.addData("lefty2", lefty2);
+        telemetry.addData("leftx2", leftx2);
+        telemetry.addData("righty2", righty2);
+        telemetry.addData("rightx2", rightx2);
+        telemetry.addData("a2", a2);
+        telemetry.addData("b2", b2);
+        telemetry.addData("x2", x2);
+        telemetry.addData("y2", y2);
+
     }
 
     public void stop() {
